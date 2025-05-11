587dde2c4ae1d1fb1a245410c38f840debefd649
Kishalaya Bhattacharjee <Kishalaya1@yahoo.com>
2025-05-11 12:23:08 +0530

    Intial logic checkin

--- Algorithms/SpecialAlgorithms.cs
+++ Algorithms/SpecialAlgorithms.cs
@@ -7,7 +7,46 @@ using System.Threading.Tasks;
 namespace Algorithms
 {
     public class SpecialAlgorithms
-    {  
-     
+    {
+
+        #region Luhn's Algorithm
+
+        /// <summary>
+        /// Validates a number using Luhn's Algorithm.
+        /// </summary>
+        /// <param name="number">The number to validate as a long</param>
+        /// <returns>True if the number is valid according to Luhn's Algorithm, otherwise false</returns>
+        public static bool IsValidLuhn(long number)
+        {
+            if (number < 0)
+                return false; // Negative numbers are not valid
+
+            int sum = 0;
+            bool doubleDigit = false;
+
+            // Process digits from right to left
+            while (number > 0)
+            {
+                int digit = (int)(number % 10); // Extract the last digit
+
+                if (doubleDigit)
+                {
+                    digit *= 2;
+                    if (digit > 9)
+                        digit -= 9; // Subtract 9 if the result is greater than 9
+                }
+
+                sum += digit;
+                doubleDigit = !doubleDigit; // Toggle the doubleDigit flag
+                number /= 10; // Remove the last digit
+            }
+
+            // The number is valid if the sum is a multiple of 10
+            return sum % 10 == 0;
+        }
+
+        #endregion
+
+
     }
 }
